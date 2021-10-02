4935485752f4bcca5715c18957f996804bacfe65
Chris Guirguis <christopher.g.codes@gmail.com>
2021-10-02 00:06:15 -0400

    Update Collections+Extensions.swift

--- Sources/SweetSimpleSwift/Extensions/Collections+Extensions.swift
+++ Sources/SweetSimpleSwift/Extensions/Collections+Extensions.swift
@@ -11,7 +11,7 @@ public extension Collection where Element: BinaryInteger {
     /// Returns the average of all elements in the array
     func average() -> Element { isEmpty ? .zero : sum() / Element(count) }
     /// Returns the average of all elements in the array as Floating Point type
-    func average<T: FloatingPoint>() -> T { isEmpty ? .zero : T(sum()) / T(count) }
+//    func average<T: FloatingPoint>() -> T { isEmpty ? .zero : T(sum()) / T(count) }
 }
 
 public extension Collection where Element: BinaryFloatingPoint {
