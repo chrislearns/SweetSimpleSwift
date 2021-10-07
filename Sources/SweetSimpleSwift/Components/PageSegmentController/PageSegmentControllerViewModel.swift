9616a684dfb0540645dab91358ea6ce1a238092b
Chris <chguirguis@mediainvestmentprojects.com>
2021-10-07 12:00:29 -0400

    added new component

---
+++ Sources/SweetSimpleSwift/Components/PageSegmentController/PageSegmentControllerViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  PageSegmentControllerViewModel.swift
+//  NewsApp
+//
+//  Created by Chris Guirguis on 10/6/21.
+//
+
+import SwiftUI
+
+open class PageSegmentControllerViewModel<T: Hashable>: ObservableObject {
+    
+    public enum Style {
+        case underline
+        case background
+    }
+    
+    public var style: Style = .background
+    public var selectedOptionBackgroundColor: Color
+    public var unselectedOptionBackgroundColor: Color
+    public var selectedOptionForegroundColor: Color
+    public var unselectedOptionForegroundColor: Color
+    public var height: CGFloat
+    public var indicatorCornerRadius: CGFloat?
+    @Published public var options: [T]
+    @Binding public var selection: T
+    
+    public init(
+        options: [T],
+        selection: Binding<T>,
+        height: CGFloat = 40,
+        indicatorCornerRadius: CGFloat? = 10,
+        style: Style,
+        selectedOptionBackgroundColor: Color = Color.blue.opacity(0.5),
+        unselectedOptionBackgroundColor: Color = .clear,
+        selectedOptionForegroundColor: Color = .black,
+        unselectedOptionForegroundColor: Color = .black
+        
+    ){
+        self.options = options
+        _selection = selection
+        self.height = height
+        self.style = style
+        self.indicatorCornerRadius = indicatorCornerRadius
+        self.selectedOptionBackgroundColor = selectedOptionBackgroundColor
+        self.unselectedOptionBackgroundColor = unselectedOptionBackgroundColor
+        self.selectedOptionForegroundColor = selectedOptionForegroundColor
+        self.unselectedOptionForegroundColor = unselectedOptionForegroundColor
+    }
+}
