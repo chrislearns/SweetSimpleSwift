b4e597940e13912ae76b7a6d0aed1d465c97ae7d
chrislearns <77995541+chrislearns@users.noreply.github.com>
2021-12-19 18:18:26 -0500

    update

--- Sources/SweetSimpleSwift/Components/PageSegmentController/ScrollingSegmentControllerView.swift
+++ Sources/SweetSimpleSwift/Components/PageSegmentController/ScrollingSegmentControllerView.swift
@@ -8,65 +8,65 @@
 import SwiftUI
 
 public struct ScrollingSegmentControllerView<T: Hashable & CustomStringConvertible>: View{
-    @ObservedObject public var viewModel: SegmentControllerViewModel<T>
-    @Binding public var selection: T
+  @ObservedObject public var viewModel: SegmentControllerViewModel<T>
+  @Binding public var selection: T
   public var options: [T]
-    @Namespace var animation
-    
+  @Namespace var animation
+  
   public init(viewModel: SegmentControllerViewModel<T>, selection: Binding<T>, options: [T]){
     self.viewModel = viewModel
     self.options = options
     _selection = selection
   }
   
-    public var body: some View {
-        ScrollView(.horizontal, showsIndicators: false){
-            
-            ScrollViewReader{proxy in
-                HStack(spacing: 8){
-                    ForEach(options, id: \.self){option in
-                        Button(action: {
-                            withAnimation(.standardEIO){
-                                selection = option
-                                print(selection)
-                            }
-                        }){
-                            Text(String(describing: option.description))
-                                .fontWeight(viewModel.fontWeight)
-                                .foregroundColor(option == selection ? .black : .init(white: 0.3))
-                                .font(.system(size: 12, weight: option == selection ? .semibold : .regular))
-                        }
-                        .padding(6)
-                        .background(
-                            ZStack{
-                                if selection == option {
-                                    if viewModel.style == .background {
-                                        viewModel.selectedOptionForegroundColor
-                                            .opacity(selection == option ? 1 : 0)
-                                            .cornerRadius(viewModel.indicatorCornerRadius ?? 0)
-                                            .matchedGeometryEffect(id: "TAB", in: animation)
-                                    } else if viewModel.style == .underline {
-                                        VStack{
-                                            Spacer()
-                                            Rectangle()
-                                                .frame(height: 3)
-                                                .foregroundColor(viewModel.indicatorColor)
-                                                .matchedGeometryEffect(id: "TAB", in: animation)
-                                        }
-                                    }
-                                }
-                            }
-                        )
-                        .id(option)
-                    }
-                }
-                .onChange(of: selection) { newValue in
-                    withAnimation{
-                    proxy.scrollTo(newValue, anchor: .center)
-                    }
-                }
+  public var body: some View {
+    ScrollView(.horizontal, showsIndicators: false){
+      
+      ScrollViewReader{proxy in
+        HStack(spacing: 8){
+          ForEach(options, id: \.self){option in
+            Button(action: {
+              withAnimation(.standardEIO){
+                selection = option
+                print(selection)
+              }
+            }){
+              Text(String(describing: option.description))
+                .fontWeight(viewModel.fontWeight)
+                .foregroundColor(option == selection ? .black : .init(white: 0.3))
+                .font(.system(size: 12, weight: option == selection ? .semibold : .regular))
             }
-        }.frame(height: viewModel.height)
-            
-    }
+            .padding(6)
+            .background(
+              ZStack{
+                if selection == option {
+                  if viewModel.style == .background {
+                    viewModel.selectedOptionForegroundColor
+                      .opacity(selection == option ? 1 : 0)
+                      .cornerRadius(viewModel.indicatorCornerRadius ?? 0)
+                      .matchedGeometryEffect(id: "TAB", in: animation)
+                  } else if viewModel.style == .underline {
+                    VStack{
+                      Spacer()
+                      Rectangle()
+                        .frame(height: 3)
+                        .foregroundColor(viewModel.indicatorColor)
+                        .matchedGeometryEffect(id: "TAB", in: animation)
+                    }
+                  }
+                }
+              }
+            )
+            .id(option)
+          }
+        }
+        .onChange(of: selection) { newValue in
+          withAnimation{
+            proxy.scrollTo(newValue, anchor: .center)
+          }
+        }
+      }
+    }.frame(height: viewModel.height)
+    
+  }
 }
