2c9ba55c7beefd4cd5d76f9c8a7e4c8fa260cf82
chrislearns <77995541+chrislearns@users.noreply.github.com>
2021-12-31 00:14:10 -0500

    Update RoundedCorner.swift

--- Sources/SweetSimpleSwift/Structs/RoundedCorner.swift
+++ Sources/SweetSimpleSwift/Structs/RoundedCorner.swift
@@ -4,21 +4,12 @@
 //
 //  Created by Christopher Guirguis on 11/23/21.
 //
-
+#if os(iOS)
 
 
 import SwiftUI
-#if os(iOS)
 import UIKit
-#elseif os(macOS)
-import AppKit
-#endif
 
-
-
-
-
-#if os(iOS)
 public struct RoundedCorner: Shape {
     public var radius: CGFloat = .infinity
     public var corners: UIRectCorner = .allCorners
@@ -28,14 +19,5 @@ public struct RoundedCorner: Shape {
         return Path(path.cgPath)
     }
 }
-#elseif os(macOS)
-public struct RoundedCorner: Shape {
-    public var radius: CGFloat = .infinity
-    public var corners: NSRect = .allCorners
-    
-    public func path(in rect: CGRect) -> Path {
-        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
-        return Path(path.cgPath)
-    }
-}
+
 #endif
